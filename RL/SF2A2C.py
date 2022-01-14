71c96ccfe39c71f6345e46d9a0ccb2f647cc3582
KahramanSel <91787796+KahramanSel@users.noreply.github.com>
2022-01-14 17:29:55 +0100

    Update SF2A2C.py

--- RL/SF2A2C.py
+++ RL/SF2A2C.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     env = DummyVecEnv([lambda: retro.make('StreetFighterIISpecialChampionEdition-Genesis', state='dhalsimvszangief', record = '.')])
     #Nutzung des A2C-Algorithmus.PPO unstabiler und langsamer in dieser Anwendung.
     model = A2C('MlpPolicy', env, verbose=1, device = 'cuda', tensorboard_log="./log_SF2/")
-    model.learn(total_timesteps=10000)
+    model.learn(total_timesteps=100000)
     model.save('SF2A2C')
     #model = A2C.load('SF2A2C')
     #model.set_env(env)
